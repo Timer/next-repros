9e1b5a234f4aa682f367ef79380a8a090b267e3f
Joe Haddad <timer150@gmail.com>
2020-01-27 13:34:50 -0500

    test

---
+++ Box6.js
@@ -0,0 +1,25 @@
+import { styled } from "linaria/react";
+import React from "react";
+
+const Box = styled.div`
+  margin-top: 40px;
+  margin-left: 40px;
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: spin 2s linear infinite;
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export default () => {
+  return <Box>6</Box>;
+};
--- pages/two.js
+++ pages/two.js
@@ -2,12 +2,9 @@ import React from "react";
 import Head from "next/head";
 import { styled } from "linaria/react";
 
-import Box1 from "../Box1";
-import Box2 from "../Box2";
-import Box3 from "../Box3";
-import Box4 from "../Box4";
-import Box5 from "../Box5";
-import Box6 from "../Box6";
+import Box7 from "../Box7";
+import Box8 from "../Box8";
+import Box9 from "../Box9";
 
 const Box = styled.div`
   margin-top: 40px;
@@ -35,12 +32,10 @@ export default () => {
         <title>With Linaria</title>
       </Head>
       <Box>Zero runtime CSS in JS</Box>
-      <Box1 />
-      <Box2 />
-      <Box3 />
-      <Box4 />
-      <Box5 />
-      <Box6 />
+
+      <Box7 />
+      <Box8 />
+      <Box9 />
     </React.Fragment>
   );
 };
